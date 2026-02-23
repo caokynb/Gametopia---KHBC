311678aba9159ea4074168cea00a2fe1bf8973f7
cClocKk <ToilaLock@users.noreply.github.com>
2026-02-23 23:51:08 +0700

    Update Fixxed Cam

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -1,23 +1,24 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject VirtualCam;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player") && !other.isTrigger)
+        // CHỈ xử lý khi vật thể chạm vào ĐÚNG là Player
+        if (other.CompareTag("Player") && !other.isTrigger)
         {
-            VirtualCam.SetActive(true);
+            VirtualCam.SetActive(true); // Bật camera của phòng này lên
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        // CHỈ xử lý khi vật thể đi ra ĐÚNG là Player
         if (other.CompareTag("Player") && !other.isTrigger)
         {
-            VirtualCam.SetActive(false);
+            VirtualCam.SetActive(false); // Tắt camera của phòng này đi khi Player rời khỏi
         }
     }
 }
